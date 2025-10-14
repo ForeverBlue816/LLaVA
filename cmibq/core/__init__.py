78aac2f1ab149a4bb70094a90e76b859d68d2aea
ForeverBlue816 <yanlchen@student.ethz.ch>
2025-10-14 08:08:48 +0200

    Add files via upload

--- cmibq/core/__init__.py
+++ cmibq/core/__init__.py
@@ -1,11 +1,12 @@
 from .ib_quantizer import IBQuantizedLayer
-from .importance_estimator import HybridImportanceEstimation
+from .importance_estimator import HybridImportanceEstimation, UniversalImportanceEstimation
 from .bit_allocator import BitAllocationNetwork
 from .differentiable_quant import DifferentiableQuantizer, StraightThroughEstimator
 
 __all__ = [
     'IBQuantizedLayer',
-    'ImportanceEstimationNetwork',
+    'HybridImportanceEstimation',  # ← 改正这里
+    'UniversalImportanceEstimation',  # ← 添加这个
     'BitAllocationNetwork', 
     'DifferentiableQuantizer',
     'StraightThroughEstimator'
